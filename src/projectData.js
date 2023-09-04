1dd4dd178962646d9cfca7d85e005dc56cb981de
kk17203 <kk17203@gmail.com>
2023-09-04 08:35:25 -0500

    fix project img path.

--- src/projectData.js
+++ src/projectData.js
@@ -2,7 +2,7 @@ export const projectData = [
     {
         name: "Tic-Tac-Toe Game",
         alt: "A screenshot of the Tic-Tac-Toe game.",
-        img: "../dist/images/tic-tac-toe.png",
+        img: "images/tic-tac-toe.png",
         description:
             "As the name suggests, this is a Tic-Tac-Toe game. It was built using HTML, CSS, and JavaScript. It was built as part of The Odin Project's curriculum.",
         techStack: "HTML, CSS, JavaScript",
@@ -12,7 +12,7 @@ export const projectData = [
     {
         name: "Weather App",
         alt: "A screenshot of the Weather App page.",
-        img: "../dist/images/weather-app.png",
+        img: "images/weather-app.png",
         description:
             "This app shows the current weather of a city. It uses the OpenWeather API to fetch the weather data. The app changes the background image based on the current weather in the area.",
         techStack: "HTML, CSS, JavaScript, OpenWeather API",
