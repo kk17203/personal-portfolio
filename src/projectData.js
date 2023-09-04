9ea2a479485e54272d15b25454e63f9a3ed3aef7
kk17203 <kk17203@gmail.com>
2023-09-04 17:07:49 -0500

    center page. fix header margin.

--- src/projectData.js
+++ src/projectData.js
@@ -1,13 +1,13 @@
 export const projectData = [
     {
-        name: "Tic-Tac-Toe Game",
-        alt: "A screenshot of the Tic-Tac-Toe game.",
-        img: "images/tic-tac-toe.png",
+        name: "Events",
+        alt: "A screenshot of the Events page.",
+        img: "images/events.png",
         description:
-            "As the name suggests, this is a Tic-Tac-Toe game. It was built using HTML, CSS, and JavaScript. It was built as part of The Odin Project's curriculum.",
+            "Events is a webpage that allows users to view, create, and delete group rides and runs. It allows users to see all events or sort events by category. ",
         techStack: "HTML, CSS, JavaScript",
-        live: "https://kk17203.github.io/tic-tac-toe/",
-        code: "https://github.com/kk17203/tic-tac-toe",
+        live: "https://kk17203.github.io/events/",
+        code: "https://github.com/kk17203/events",
     },
     {
         name: "Weather App",
@@ -18,5 +18,15 @@ export const projectData = [
         techStack: "HTML, CSS, JavaScript, OpenWeather API",
         live: "https://kk17203.github.io/weather-page/",
         code: "https://github.com/kk17203/weather-page",
+    },
+    {
+        name: "Tic-Tac-Toe Game",
+        alt: "A screenshot of the Tic-Tac-Toe game.",
+        img: "images/tic-tac-toe.png",
+        description:
+            "As the name suggests, this is a Tic-Tac-Toe game. It was built using HTML, CSS, and JavaScript. It was built as part of The Odin Project's curriculum.",
+        techStack: "HTML, CSS, JavaScript",
+        live: "https://kk17203.github.io/tic-tac-toe/",
+        code: "https://github.com/kk17203/tic-tac-toe",
     },
 ];
--- src/projectRender.js
+++ src/projectRender.js
@@ -1,61 +1,26 @@
 import { projectData } from "./projectData";
 
 function renderProjects(num) {
-    const projects = document.querySelector(".projects");
-
     for (let i = 0; i < num; i++) {
         const project = projectData[i];
-
         const projectCard = document.createElement("div");
         projectCard.classList.add("project-card");
         projectCard.setAttribute("data-index", i);
-        projects.appendChild(projectCard);
-
-        let projectImg = document.createElement("div");
-        projectImg.classList.add("project-img");
-        projectCard.appendChild(projectImg);
-
-        let image = document.createElement("img");
-        image.src = project.img;
-        image.alt = project.alt;
-        projectImg.appendChild(image);
-
-        let projectInfo = document.createElement("div");
-        projectInfo.classList.add("project-info");
-        projectCard.appendChild(projectInfo);
-
-        let projectName = document.createElement("h2");
-        projectName.classList.add("project-name");
-        projectName.textContent = project.name;
-        projectInfo.appendChild(projectName);
-
-        let projectTechStack = document.createElement("p");
-        projectTechStack.classList.add("project-tech-stack");
-        projectTechStack.innerHTML = `<span>Tech Stack: </span>${project.techStack}`;
-        projectInfo.appendChild(projectTechStack);
-
-        let projectDescription = document.createElement("p");
-        projectDescription.classList.add("project-description");
-        projectDescription.textContent = project.description;
-        projectInfo.appendChild(projectDescription);
-
-        let projectLinks = document.createElement("div");
-        projectLinks.classList.add("project-links");
-        projectInfo.appendChild(projectLinks);
-
-        let liveLink = document.createElement("a");
-        liveLink.href = project.live;
-        liveLink.target = "_blank";
-        liveLink.rel = "noopener noreferrer";
-        liveLink.textContent = "Live";
-        projectLinks.appendChild(liveLink);
-
-        let codeLink = document.createElement("a");
-        codeLink.href = project.code;
-        codeLink.target = "_blank";
-        codeLink.rel = "noopener noreferrer";
-        codeLink.textContent = "Code";
-        projectLinks.appendChild(codeLink);
+        projectCard.innerHTML = `
+        <div class="project-img-container">
+            <img src="${project.img}" alt="${project.alt}">
+        </div>
+        <div class="project-info">
+            <h2 class="project-name">${project.name}</h2>
+            <p class="project-tech-stack"><span>Tech Stack: </span>${project.techStack}</p>
+            <p class="project-description">${project.description}</p>
+            <div class="project-links">
+                <a href="${project.live}" target="_blank" rel="noopener noreferrer">Live</a>
+                <a href="${project.code}" target="_blank" rel="noopener noreferrer">Code</a>
+            </div>
+        </div>
+        `;
+        document.querySelector(".projects").appendChild(projectCard);
     }
 }
 
