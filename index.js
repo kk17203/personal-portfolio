a57ff2637721a01e83c471edb4865b7294d56afc
kk17203 <kk17203@gmail.com>
2023-09-04 06:33:49 -0500

    add linux to tech and tools. add JS to randomly select header img.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+const headerImg = document.querySelector(".header-img");
+
+const headerImages = [
+    "images/cross-roads.jpg",
+    "images/trees.jpg",
+    "images/bridge.jpg",
+    "images/road.jpg",
+];
+
+const randomIndex = Math.floor(Math.random() * headerImages.length);
+
+headerImg.src = headerImages[randomIndex];
