d7282178750f5e65f173cd364e938e48df625040
kk17203 <kk17203@gmail.com>
2023-10-29 05:28:34 -0500

    Deleted console logs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,4 @@ import headerImg from "./headerImg";
 import { projectData } from "./projectData";
 import renderProjects from "./projectRender";
 
-console.log(projectData[0].description);
-console.log(projectData[0].alt);
-
 renderProjects(projectData.length);
