b5e87f28976353601b679af55ff0aaea3ff9de0d
kk17203 <kk17203@gmail.com>
2023-10-29 06:54:21 -0500

    Resized header images.

--- src/headerImg.js
+++ src/headerImg.js
@@ -1,10 +1,12 @@
 const headerImg = document.querySelector(".header-img");
 
 const headerImages = [
-    "images/cross-roads.jpg",
-    "images/trees.jpg",
-    "images/bridge.jpg",
-    "images/road.jpg",
+    "images/cross-roads-2.jpg",
+    // "images/trees.jpg",
+    // "images/bridge.jpg",
+    // "images/road.jpg",
+    "images/abandoned-road.jpg",
+    "images/silo.jpg",
 ];
 
 const randomIndex = Math.floor(Math.random() * headerImages.length);
